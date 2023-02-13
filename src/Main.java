d6c2fd5c2e58f75edad3f2953f3b63586064517c
Артем <artemgolyshkov@mail.ru>
2023-02-13 22:37:03 +0300

    Домашняя работа №2 задачи 1-5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        public static void main(String[] args) {
+            task1();
+            task2();
+            task3();
+        }
+
+        public static void task1 () {
+            System.out.println("Задача 1-3");
+            System.out.println("Задача 1");
+              var dog = 8.0;
+               System.out.println(dog);
+              var cat = 3.6;
+               System.out.println(cat);
+              var paper = 763789;
+               System.out.println(paper); // Код для задачи 1
+            System.out.println("Задача 2");
+              dog = dog + 4;
+               System.out.println(dog);
+              cat = cat + 4;
+               System.out.println(cat);
+              paper = paper + 4;
+               System.out.println(paper);// Код для задачи 2
+            System.out.println("Задача 3");
+              dog = dog - 3.5 ;
+               System.out.println(dog);
+              cat = cat - 1.6;
+               System.out.println(cat);
+              paper = paper - 7639;
+               System.out.println(paper);// Код для задачи 3
+
+        }
+
+        public static void task2 () {
+            System.out.println("Задача 4-5");
+            System.out.println("Задача 4");
+              var friend = 19;
+               System.out.println(friend);
+              friend = friend + 2;
+               System.out.println(friend);
+              friend = friend / 7;
+               System.out.println(friend);// Код для задачи 4
+            System.out.println("Задача 5");
+              var frog = 3.5;
+               System.out.println(frog);
+              frog = frog * 10;
+               System.out.println(frog);
+              frog = frog / 3.5;
+               System.out.println(frog);
+              frog = frog + 4;
+               System.out.println(frog);// Код для задачи 4
+
+        }
+    public static void task3 () {
+        System.out.println("Задача 6-8");
+        var dog = 8.0 + 4;
+        System.out.println(dog);
+        var cat = 3.6 + 4;
+        System.out.println(cat);
+        var paper = 763789 + 4;
+        System.out.println(paper);// Код для задачи 3
+
     }
-}
+    }
